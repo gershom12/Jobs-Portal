d86a16c34a1909dfdff0e199cebb952209a90c36
gershom12 <u13229908@tuks.co.za>
2018-05-21 14:55:22 -0700

    Initial project checkin

---
+++ Jobs-Blog/src/main/java/com/chowers/jobs/blog/entities/Comment.java
@@ -0,0 +1,31 @@
+package com.chowers.jobs.blog.entities;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * Created by mghasemy on 12/10/16.
+ */
+@javax.persistence.Entity
+public class Comment implements Entity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String text;
+
+    @Column
+    private Long blogid;
+
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ Jobs-Blog/src/main/java/com/chowers/jobs/blog/entities/Entity.java
@@ -0,0 +1,8 @@
+package com.chowers.jobs.blog.entities;
+
+import java.io.Serializable;
+
+public interface Entity extends Serializable
+{
+    Long getId();
+}
